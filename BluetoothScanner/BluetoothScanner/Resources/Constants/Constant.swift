3e3872be285d73f762cd3f0591ce9a6ca81c0aac
gayeong <lgy0530@naver.com>
2023-08-24 16:17:21 +0900

    [CHORE] - 고정값으로 변경

---
+++ BluetoothScanner/BluetoothScanner/Resources/Constants/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  BluetoothScanner
+//
+//
+
+import Foundation
+
+enum Bluetooth {
+    static var BLUETOOTH_SERIAL_NAME: String = ""
+    static var BLUETOOTH_SCAN_TIME: Int = 10
+    static var BLUETOOTH_LOTTIE_NAME: String = "lottie_loading"
+}
